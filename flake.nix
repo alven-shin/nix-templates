bb71c9f54e115487f62bcbefea2632388f816be2
Alvin Shen <29553973+alven-shin@users.noreply.github.com>
2025-01-21 10:05:57 -0500

    feat: remove other shell templates

--- flake.nix
+++ flake.nix
@@ -11,27 +11,9 @@
         path = ./cpp;
         description = "C++ template with CMake, adapted from https://gitlab.com/CLIUtils/modern-cmake/.";
       };
-
-      shells = {
-        python = {
-          path = ./shells/python;
-          description = "Python development shell template, using pdm.";
-        };
-
-        java = {
-          path = ./shells/java;
-          description = "Java development shell template, using gradle.";
-        };
-
-        csharp = {
-          path = ./shells/csharp;
-          description = "Dotnet and C# development shell template.";
-        };
-
-        generic = {
-          path = ./shells/generic;
-          description = "Generic development shell template.";
-        };
+      generic = {
+        path = ./generic;
+        description = "Generic development shell template.";
       };
     };
   };
